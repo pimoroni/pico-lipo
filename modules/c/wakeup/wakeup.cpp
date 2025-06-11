18f0c9dab2366a8b9848d7b2baa55994fa422667
Phil Howard <github@gadgetoid.com>
2025-06-11 11:03:14 +0100

    wakeup: Use 32bit functions for RP2040.

--- modules/c/wakeup/wakeup.cpp
+++ modules/c/wakeup/wakeup.cpp
@@ -1,14 +1,24 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 
-
+#if PICO_NUM_GPIOS >= 32
 uint64_t gpio_state = 0;
+#else
+uint32_t gpio_state = 0;
+#endif
 
 static void __attribute__((constructor(101))) latch_inputs() {
+#if PICO_NUM_GPIOS >= 32
     gpio_set_function_masked64(0xffffffffffffffff, GPIO_FUNC_SIO);
     gpio_state = gpio_get_all64();
     sleep_ms(5);
     gpio_state |= gpio_get_all64();
+#else
+    gpio_set_function_masked(0xffffffff, GPIO_FUNC_SIO);
+    gpio_state = gpio_get_all();
+    sleep_ms(5);
+    gpio_state |= gpio_get_all();
+#endif
 }
 
 extern "C" {
