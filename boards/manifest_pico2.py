e0e12ca597a4c6cb717735a687b448bb0dcff3d1
Phil Howard <github@gadgetoid.com>
2025-07-16 15:58:05 +0100

    modules: vendor ezwifi.

--- boards/manifest_pico2.py
+++ boards/manifest_pico2.py
@@ -1,5 +1,6 @@
 # micropython/ports/rp2/../../../
 MODULES_PY = "$(PORT_DIR)/../../../pimoroni-pico/micropython/modules_py"
+MODULES_LOCAL = "$(BOARD_DIR)/../../modules/python"
 
 # SD Card
 require("sdcard")
@@ -10,3 +11,5 @@ freeze(MODULES_PY, "pimoroni.py")
 freeze(MODULES_PY, "boot.py")
 
 freeze(MODULES_PY, "lte.py")
+
+freeze(MODULES_LOCAL, "ezwifi.py")
