3182066d678bc3c609257553db9c3615e0713b61
Phil Howard <github@gadgetoid.com>
2025-07-16 15:35:05 +0100

    examples: Add with-examples manifest.

--- boards/pimoroni_pico_lipo2xl_w/mpconfigboard.cmake
+++ boards/pimoroni_pico_lipo2xl_w/mpconfigboard.cmake
@@ -16,4 +16,6 @@ include(enable_cyw43.cmake)
 # Board specific version of the frozen manifest
 set(MICROPY_FROZEN_MANIFEST ${MICROPY_BOARD_DIR}/manifest.py)
 
+set(PIMORONI_UF2_MANIFEST ${CMAKE_CURRENT_LIST_DIR}/manifest.txt)
+set(PIMORONI_UF2_DIR ${CMAKE_CURRENT_LIST_DIR}/../../examples)
 include(${CMAKE_CURRENT_LIST_DIR}/../common.cmake)
